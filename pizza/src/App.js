8fca8d597c83d6ef32c25fd9868bed1b1fee18f9
Dimdim28 <wwwdimanes1@gmail.com>
2022-07-11 21:11:08 +0300

    Adding render components from file

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -1,7 +1,30 @@
+import Header from "./components/Header";
+import "./scss/app.scss";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+import pizzas from "../src/assets/pizzas.json";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzas.map((obj) => (
+                <PizzaBlock {...obj} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ pizza/src/components/Categories.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const array = ["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {array.map((elem, ind) => (
+          <li
+            key={ind}
+            onClick={() => onClickCategory(ind)}
+            className={activeIndex === ind ? "active" : ""}
+          >
+            {elem}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
