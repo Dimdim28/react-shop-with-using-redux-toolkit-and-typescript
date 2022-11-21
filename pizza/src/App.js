01a301bdc4e0bd1dcf80326dc41cb78208cfbd83
Dimdim28 <wwwdimanes1@gmail.com>
2022-11-21 22:25:39 +0200

    Created Search component

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
@@ -7,13 +8,14 @@ import NotFound from "./pages/NotFound";
 import "./scss/app.scss";
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState("");
   return (
     <div className="App">
       <div className="wrapper">
-        <Header />
+        <Header searchValue={searchValue} setSearchValue={setSearchValue} />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home searchValue={searchValue} />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
--- pizza/src/pages/Home.jsx
+++ pizza/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import { Skeleton } from "../components/PizzaBlock/Skeleton";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
@@ -31,6 +31,15 @@ const Home = () => {
     window.scrollTo(0, 0);
   }, [categoryId, sortType]);
 
+  const pizzas = items
+    .filter((obj) =>
+      obj.title.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const skeleton = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
+
   return (
     <div className="container">
       <div className="content__top">
@@ -43,11 +52,7 @@ const Home = () => {
         <Sort value={sortType} onChangeSort={(type) => setSortType(type)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeleton : pizzas}</div>
     </div>
   );
 };
