495b14e5035973a48bf9e8c7f4efea447dca9d7f
Dimdim28 <wwwdimanes1@gmail.com>
2022-11-28 22:44:40 +0200

    Updates

--- pizza/src/redux/cart/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from '../store';
-
-export const selectCart = (state: RootState) => state.cart;
-export const selectCartItemsById = (id: string) => (state: RootState) =>
-    state.cart.items.find((obj) => obj.id === id);
--- pizza/src/redux/filter/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from './../store';
-
-export const selectSort = (state: RootState) => state.filter.sort;
-export const selectFilter = (state: RootState) => state.filter;
-
